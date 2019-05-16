a7cf2059e1c68c0f9b31b51e6b05387ef104463c
Troy Neubauer <troyneubauer@gmail.com>
2019-05-15 22:45:56 -0400

    Added massive THS role list

---
+++ public/js/roles.js
@@ -0,0 +1,226 @@
+let roles = [
+"Acting",
+{
+	name: "Major Role",
+	oneAct: 4,
+	fullLength: 8
+},{
+	name: "Minor Role",
+	oneAct: 3,
+	fullLength: 5
+},{
+	name: "Walk-On",
+	oneAct: 1,
+	fullLength: 2
+},{
+	name: "Chorus",
+	oneAct: 1,
+	fullLength: 3
+},{
+	name: "Dancer",
+	oneAct: 1,
+	fullLength: 3
+},{
+	name: "Understudy",
+	oneAct: 1,
+	fullLength: 2
+},
+"Production",
+{
+	name: "Stage Manager",
+	oneAct: 4,
+	fullLength: 8
+},{
+	name: "Stage Crew",
+	oneAct: 2,
+	fullLength: 4
+},{
+	name: "Lighting Technician",
+	oneAct: 3,
+	fullLength: 6
+},{
+	name: "Lighting Crew",
+	oneAct: 2,
+	fullLength: 3
+},{
+	name: "Set Designer",
+	oneAct: 4,
+	fullLength: 5
+},{
+	name: "Set Construction Crew",
+	oneAct: 3,
+	fullLength: 5
+},{
+	name: "Costumer",
+	oneAct: 3,
+	fullLength: 6
+},{
+	name: "Costume Crew",
+	oneAct: 2,
+	fullLength: 5
+},{
+	name: "Properties Manager",
+	oneAct: 3,
+	fullLength: 5
+},{
+	name: "Properties Crew",
+	oneAct: 2,
+	fullLength: 3
+},{
+	name: "Sound Technician",
+	oneAct: 3,
+	fullLength: 5
+},{
+	name: "Sound Crew",
+	oneAct: 2,
+	fullLength: 3
+},{
+	name: "Video Editor",
+	oneAct: 1.5,
+	fullLength: 2
+},{
+	name: "Video Crew",
+	oneAct: 1.5,
+	fullLength: 2
+},{
+	name: "Makeup Manager",
+	oneAct: 3,
+	fullLength: 5
+},{
+	name: "Rehearsal Prompter",
+	oneAct: 2,
+	fullLength: 4
+},{
+	name: "Pianist",
+	oneAct: 3,
+	fullLength: 6
+},{
+	name: "Musicians",
+	oneAct: 2,
+	fullLength: 3
+},
+"Business",
+{
+	name: "Business Manager",
+	oneAct: 4,
+	fullLength: 6
+},{
+	name: "Business Crew",
+	oneAct: 2,
+	fullLength: 4
+},{
+	name: "Publicity Manager",
+	oneAct: 3,
+	fullLength: 5
+},{
+	name: "Publicity Crew",
+	oneAct: 2,
+	fullLength: 3
+},{
+	name: "Ticket Manager",
+	oneAct: 2,
+	fullLength: 4
+},{
+	name: "Ticket Crew",
+	oneAct: 1,
+	fullLength: 3
+},{
+	name: "House Manager",
+	oneAct: 2,
+	fullLength: 4
+},{
+	name: "House Crew",
+	oneAct: 1,
+	fullLength: 2
+},{
+	name: "Usher",
+	oneAct: 1,
+	fullLength: 2
+},{
+	name: "Programs",
+	oneAct: 1,
+	fullLength: 3
+},{
+	name: "Programs Crew",
+	oneAct: 1,
+	fullLength: 2
+},"Directing",{
+	name: "Director",
+	oneAct: 4,
+	fullLength: 8
+},{
+	name: "Assistant Director",
+	oneAct: 3,
+	fullLength: 6
+},{
+	name: "Vocal Director",
+	oneAct: 3,
+	fullLength: 6
+},{
+	name: "Video Producer/Director",
+	oneAct: 3,
+	fullLength: 4
+},{
+	name: "Assistant Vocal Director",
+	oneAct: 2,
+	fullLength: 5
+},{
+	name: "Orchestra or Band Director",
+	oneAct: 3,
+	fullLength: 6
+},{
+	name: "Assistant Orchestra or Band Director",
+	oneAct: 2,
+	fullLength: 5
+},{
+	name: "Choreographer",
+	oneAct: 4,
+	fullLength: 7
+},{
+	name: "Assistant Choreographer",
+	oneAct: 3,
+	fullLength: 5
+},"Writing",{
+	name: "Origional Play (Produced)",
+	oneAct: 5,
+	fullLength: 8
+},{
+	name: "Origional Radio Script (Produced)",
+	oneAct: 4,
+	fullLength: 6
+},{
+	name: "Origional TV (Produced)",
+	oneAct: 4,
+	fullLength: 6
+},{
+	name: "Origional Play (Unproduced)",
+	oneAct: 1,
+	fullLength: 2
+},{
+	name: "Origional Radio Script (Unproduced)",
+	oneAct: 0.5,
+	fullLength: 1.5
+},{
+	name: "Origional TV Script (Unproduced)",
+	oneAct: 0.5,
+	fullLength: 1.5
+},
+"Miscellaneous",
+{
+	name: "Oral Interoretation",
+	value: 2
+},{
+	name: "Duet Acting Scenes",
+	value: 2
+},{
+	name: "Participation in Theature Festival or Contest",
+	value: 3
+},{
+	name: "Atting Theature Festival",
+	value: 1,
+	rate: "day"
+},{
+	name: "Assembly Program",
+	value: 0.1,
+	rate: "hour"
+}]
