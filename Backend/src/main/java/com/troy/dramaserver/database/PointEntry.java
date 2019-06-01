702e9f0b5c6a66ecb5ef67c7bc4ba6125200891d
Troy Neubauer <troyneubauer@gmail.com>
2019-06-01 08:01:01 -0400

    Finished add page

--- Backend/src/main/java/com/troy/dramaserver/database/PointEntry.java
+++ Backend/src/main/java/com/troy/dramaserver/database/PointEntry.java
@@ -5,13 +5,37 @@ import java.io.Serializable;
 public class PointEntry implements Serializable {
 
 	private long userID;
-	private String category, role;
+	private String category, role, info;
 	private PointsStorage points;
+	
+	
+	public PointEntry(long userID, String category, String role, String info, int amount, boolean oneAct) {
+		this.userID = userID;
+		this.category = category;
+		this.role = role;
+		this.info = info;
+		this.points = new SinglePoints(amount, oneAct);
+	}
+	
+	public PointEntry(long userID, String category, String role, String info, int points) {
+		this.userID = userID;
+		this.category = category;
+		this.role = role;
+		this.info = info;
+		this.points = new RatePoints(points);
+	}
+	
+	public PointEntry(long userID, String category, String role, String info, double rate, int amount, String rateString) {
+		this.userID = userID;
+		this.category = category;
+		this.role = role;
+		this.info = info;
+		this.points = new RatePoints(rate, amount, rateString);
+	}
 
-	private interface PointsStorage {
-
-		public double getPoints();
-		public String toString();
+	@Override
+	public String toString() {
+		return points.toString() + " " + category + " as " + role;
 	}
 
 	protected static String getPointsString(double points) {
@@ -20,17 +44,33 @@ public class PointEntry implements Serializable {
 		else
 			return "points";
 	}
+	
+	
+	
+	
+	private interface PointsStorage {
+
+		public double getPoints();
+
+		public String toString();
+	}
 
 	public class SinglePoints implements PointsStorage {
 		private int amount;
+		boolean oneAct;
 
-		public SinglePoints(int amount) {
+		public SinglePoints(int amount, boolean oneAct) {
 			this.amount = amount;
+			this.oneAct = oneAct;
 		}
 
 		@Override
 		public double getPoints() {
 			return amount;
+		}
+		
+		public boolean isOneAct() {
+			return oneAct;
 		}
 
 		@Override
@@ -40,9 +80,13 @@ public class PointEntry implements Serializable {
 	}
 
 	public class RatePoints implements PointsStorage {
-		private double rate;
+		private double rate;// When rate is 0 this indicates a one time point collection equal to amount
 		private int amount;
 		private String rateString;
+		
+		public RatePoints(int points) {
+			this(0.0, points, null);
+		}
 
 		public RatePoints(double rate, int amount, String rateString) {
 			this.rate = rate;
@@ -52,12 +96,18 @@ public class PointEntry implements Serializable {
 
 		@Override
 		public double getPoints() {
-			return rate * (double) amount;
+			if (rate == 0.0)
+				return amount;
+			else
+				return rate * (double) amount;
 		}
 
 		@Override
 		public String toString() {
-			return getPoints() + getPointsString(getPoints()) + " " + amount + " " + rateString + "s at " + rate + " " + getPointsString(rate) + " per " + rateString;
+			if (rate == 0.0)
+				return getPoints() + getPointsString(getPoints());
+			else
+				return getPoints() + getPointsString(getPoints()) + " " + amount + " " + rateString + "s at " + rate + " " + getPointsString(rate) + " per " + rateString;
 		}
 	}
 
