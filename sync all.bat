7a17d4295889bbac1978eaec7feb59441e20dcd8
Troy Neubauer <troyneubauer@gmail.com>
2019-06-02 12:00:31 -0400

    removed old files

--- sync all.bat
+++
@@ -1,2 +0,0 @@
-pscp -i "C:\AWS\Drama.ppk" "C:\Users\Troy Neubauer\Desktop\Drama\Server.js" ubuntu@ec2-3-14-39-226.us-east-2.compute.amazonaws.com:/home/ubuntu/Server.js
-pscp -i "C:\AWS\Drama.ppk" -r "C:\Users\Troy Neubauer\Desktop\Drama\public" ubuntu@ec2-3-14-39-226.us-east-2.compute.amazonaws.com:/home/ubuntu
--- sync.bat
+++
@@ -1,1 +0,0 @@
-pscp -i "C:\AWS\Drama.ppk" "C:\Users\Troy Neubauer\Desktop\Drama\Server.js" ubuntu@ec2-3-14-39-226.us-east-2.compute.amazonaws.com:/home/ubuntu/Server.js
